f8d6c34df6da53b91516990b3f5500b26b5a0d9b
JesseBennett-lab <j.bennett0505@gmail.com>
2026-06-09 14:59:07 +0200

    updated ids

--- app/src/main/java/com/example/travelpackingapp/MainActivity.kt
+++ app/src/main/java/com/example/travelpackingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.travelpackingapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val exitButton = findViewById<Button>(R.id.exitButton)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val quantityText = findViewById<EditText>(R.id.quantityEditText)
+
+        addButton.setOnClcikListener {
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
